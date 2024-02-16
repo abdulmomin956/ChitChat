5f98186c78b991ab70f9b5a5c7788a33231f676a
Abdul Momin <66585914+abdulmomin956@users.noreply.github.com>
2024-02-16 12:15:31 +0600

    remoed websocket only setting for socket io

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,6 @@ const Home = ({ isAuth, auth }) => {
     if (auth?._id && auth?.username) {
       socket.current = io('/', {
         path: '/api/socket',
-        transports: ['websocket'],
         query: `username=${auth.username}&id=${auth._id}`
       })
     }
