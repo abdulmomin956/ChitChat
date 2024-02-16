9ff1ae69ff2e2eb222964733a24de0e5381323c9
Abdul Momin <66585914+abdulmomin956@users.noreply.github.com>
2024-02-16 11:18:50 +0600

    connection error on client console

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,6 +22,19 @@ const Home = ({ isAuth, auth }) => {
     }
   }, [auth?._id, auth?.username])
 
+  useEffect(() => {
+    socket?.current?.on("connect_error", (err) => {
+      // the reason of the error, for example "xhr poll error"
+      console.log(err.message);
+
+      // some additional description, for example the status code of the initial HTTP response
+      console.log(err.description);
+
+      // some additional context, for example the XMLHttpRequest object
+      console.log(err.context);
+    });
+  }, [])
+
   // console.log(auth);
 
   const conversions = [
